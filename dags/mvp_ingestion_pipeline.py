3f85ebdaa51133e96249aa7df1a8bd80384d2a02
Luís Gabriel Nascimento Simas <luis.gabriel_thera@prestador.globo>
2025-12-19 02:02:11 -0300

    fix: Ajuste de inciailização do Airflow webserver

--- dags/mvp_ingestion_pipeline.py
+++ dags/mvp_ingestion_pipeline.py
@@ -5,5 +5,5 @@ from modules.mvp_orchestrator_dag import PipelineOrchestrator
 orchestrator = PipelineOrchestrator()
 dag = orchestrator.create_dag(
     dag_id='mvp_ingestion_pipeline_v2',
-    schedule_interval='@daily'
+    schedule_interval=None
 )
